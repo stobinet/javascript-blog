306ad2c34535308db6d7f17c6ef83075c10f48cd
stobin <stobin@stobin.net>
2019-03-05 00:46:10 +0100

    Find the correct article using the selector

--- js/script.js
+++ js/script.js
@@ -29,7 +29,9 @@ function titleClickHandler(event) {
     const articleSelector = clickedElement.getAttribute("href");
     console.log(articleSelector);
 
-    /* [TO-DO] find the correct article using the selector (value of 'href' attribute) */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
 
     /* [TO-DO] add class 'active' to the correct article */
 }
