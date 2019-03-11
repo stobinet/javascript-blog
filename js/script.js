7d40abb60c2e4bdcc2fd277153caf7dc72ebacdd
stobin <stobin@stobin.net>
2019-03-11 19:22:17 +0100

    Generate tags into html

--- js/script.js
+++ js/script.js
@@ -93,29 +93,46 @@ function generateTitleLinks() {
 generateTitleLinks();
 
 function generateTags() {
+  console.log('wywołano funkcję generateTags');
   /* find all articles */
+  const articles = document.querySelectorAll(optArticleSelector);
 
   /* START LOOP: for every article: */
+  for (let article of articles) {
 
-  /* find tags wrapper */
+    /* find tags wrapper */
+    const tagsWrapper = article.querySelector(optArticleTagsSelector);
 
-  /* make html variable with empty string */
+    /* make html variable with empty string */
+    let html = '';
 
-  /* get tags from data-tags attribute */
+    /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute("data-tags");
+    console.log(articleTags);
 
-  /* split tags into array */
+    /* split tags into array */
+    const articleTagsArray = articleTags.split(' ');
+    console.log(articleTagsArray);
 
-  /* START LOOP: for each tag */
+    /* START LOOP: for each tag */
+    for (let tag of articleTagsArray) {
+      console.log(tag);
 
-  /* generate HTML of the link */
+      /* generate HTML of the link */
+      const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li> ';
+      console.log(linkHTML);
 
-  /* add generated code to html variable */
+      /* add generated code to html variable */
+      html = html + linkHTML;
+      console.log(html);
 
-  /* END LOOP: for each tag */
+    }/* END LOOP: for each tag */
 
-  /* insert HTML of all the links into the tags wrapper */
+    /* insert HTML of all the links into the tags wrapper */
+    tagsWrapper.innerHTML = html;
+    console.log(tagsWrapper);
 
-  /* END LOOP: for every article: */
+  } /* END LOOP: for every article: */
 }
 
 generateTags();
