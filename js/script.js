302fc49af08985621aff7a0580c230a33723644c
stobin <stobin@stobin.net>
2019-03-11 20:40:28 +0100

    Add authors to the articles

--- js/script.js
+++ js/script.js
@@ -41,6 +41,7 @@ function titleClickHandler(event) {
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
+  optArticleAuthorSelector = '.post-author',
   optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks(customSelector = '') {
@@ -98,41 +99,41 @@ generateTitleLinks();
 function generateTags() {
   console.log('wywołano funkcję generateTags');
 
-  /* find all articles */
+  /* [DONE] find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
 
   /* START LOOP: for every article: */
   for (let article of articles) {
 
-    /* find tags wrapper */
+    /* [DONE] find tags wrapper */
     const tagsWrapper = article.querySelector(optArticleTagsSelector);
 
-    /* make html variable with empty string */
+    /* [DONE] make html variable with empty string */
     let html = '';
 
-    /* get tags from data-tags attribute */
+    /* [DONE] get tags from data-tags attribute */
     const articleTags = article.getAttribute("data-tags");
     console.log(articleTags);
 
-    /* split tags into array */
+    /* [DONE] split tags into array */
     const articleTagsArray = articleTags.split(' ');
     console.log(articleTagsArray);
 
-    /* START LOOP: for each tag */
+    /* [DONE] START LOOP: for each tag */
     for (let tag of articleTagsArray) {
       console.log(tag);
 
-      /* generate HTML of the link */
+      /* [DONE] generate HTML of the link */
       const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li> ';
       console.log(linkHTML);
 
-      /* add generated code to html variable */
+      /* [DONE] add generated code to html variable */
       html = html + linkHTML;
       console.log(html);
 
     }/* END LOOP: for each tag */
 
-    /* insert HTML of all the links into the tags wrapper */
+    /* [DONE] insert HTML of all the links into the tags wrapper */
     tagsWrapper.innerHTML = html;
     console.log(tagsWrapper);
 
@@ -146,19 +147,19 @@ function tagClickHandler(event) {
   /* prevent default action for this event */
   event.preventDefault();
 
-  /* make new constant named "clickedElement" and give it the value of "this" */
+  /* [DONE] make new constant named "clickedElement" and give it the value of "this" */
   const clickedElement = this;
   console.log('Link was clicked!');
 
-  /* make a new constant "href" and read the attribute "href" of the clicked element */
+  /* [DONE] make a new constant "href" and read the attribute "href" of the clicked element */
   const href = clickedElement.getAttribute("href");
   console.log(href);
 
-  /* make a new constant "tag" and extract tag from the "href" constant */
+  /* [DONE] make a new constant "tag" and extract tag from the "href" constant */
   const tag = href.replace('#tag-', '');
   console.log(tag);
 
-  /* find all tag links with class active */
+  /* [DONE] find all tag links with class active */
   const links = document.querySelectorAll('a.active[href^="#tag-"]'); //??? NodeList [], length: 0;
   console.log(links);
 
@@ -206,3 +207,40 @@ function addClickListenersToTags() {
 }
 
 addClickListenersToTags();
+
+function generateAuthors() {
+  console.log('wywołano funkcję generateAuthors');
+
+  /* [DONE] find all articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  /* START LOOP: for every article: */
+  for (let article of articles) {
+
+      /* [DONE] find tag wrapper */
+      const tagWrapper = article.querySelector(optArticleAuthorSelector);
+
+      /* [DONE] make html variable with empty string */
+      let html = '';
+
+      /* [DONE] get author from data-author attribute */
+      const author = article.getAttribute("data-author");
+      console.log(author);
+
+      /* [DONE] generate HTML of the link */
+      const linkHTML = 'by ' + '<a href="#' + author + '">' + author + '</a>';
+      console.log(linkHTML);
+
+      /* [DONE] add generated code to html variable */
+      html = html + linkHTML;
+      console.log(html);
+
+      /* [DONE] insert HTML of all the links into the tags wrapper */
+      tagWrapper.innerHTML = html;
+      console.log(tagWrapper);
+
+  } /* END LOOP: for every article: */
+
+}
+
+generateAuthors();
